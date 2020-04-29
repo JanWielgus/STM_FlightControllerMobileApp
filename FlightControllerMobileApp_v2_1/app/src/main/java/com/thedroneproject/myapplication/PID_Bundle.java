316a6c0555d8dfa2193b902da2b0617943456eab
JanWielgus <jan.wielgus12@gmail.com>
2020-04-29 12:46:35 +0200

    Prepated files for PID tuning activity

---
+++ FlightControllerMobileApp_v2_1/app/src/main/java/com/thedroneproject/myapplication/PID_Bundle.java
@@ -0,0 +1,63 @@
+package com.thedroneproject.myapplication;
+
+public class PID_Bundle
+{
+    private float kP;
+    private float kI;
+    private float kD;
+    private int iMax;
+
+    public PID_Bundle()
+    {
+        kP = 0;
+        kI = 0;
+        kD = 0;
+        iMax = 0;
+    }
+
+
+    // Getters
+
+    public float getkP() {
+        return kP;
+    }
+
+    public float getkI() {
+        return kI;
+    }
+
+    public float getkD() {
+        return kD;
+    }
+
+    public int getiMax() {
+        return iMax;
+    }
+
+
+    // Setters
+
+    public void setkP(float kP) {
+        this.kP = kP;
+    }
+
+    public void setkI(float kI) {
+        this.kI = kI;
+    }
+
+    public void setkD(float kD) {
+        this.kD = kD;
+    }
+
+    public void setiMax(int iMax) {
+        this.iMax = iMax;
+    }
+
+    public void set(float kP, float kI, float kD, int iMax)
+    {
+        setkP(kP);
+        setkI(kI);
+        setkD(kD);
+        setiMax(iMax);
+    }
+}
---
+++ FlightControllerMobileApp_v2_1/app/src/main/java/com/thedroneproject/myapplication/PID_TuningActivity.java
@@ -0,0 +1,17 @@
+package com.thedroneproject.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class PID_TuningActivity extends Activity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pid_tuning_activity);
+
+    }
+}
