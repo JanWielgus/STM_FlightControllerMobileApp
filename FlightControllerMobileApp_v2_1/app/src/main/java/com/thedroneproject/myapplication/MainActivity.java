e1b6397106a4984788b09e67ef253e04a0832247
JanWielgus <jan.wielgus12@gmail.com>
2020-04-29 13:19:11 +0200

    Added menu and changin activity option

--- FlightControllerMobileApp_v2_1/app/src/main/java/com/thedroneproject/myapplication/MainActivity.java
+++ FlightControllerMobileApp_v2_1/app/src/main/java/com/thedroneproject/myapplication/MainActivity.java
@@ -1,14 +1,42 @@
 package com.thedroneproject.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.PID_tuning_menu_item)
+        {
+            Intent PID_TuningIntent = new Intent(this, PID_TuningActivity.class);
+            startActivity(PID_TuningIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
